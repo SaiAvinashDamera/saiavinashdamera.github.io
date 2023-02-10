64f18f54c694c52b7f0d20148b418a92af3cae9e
SaiAvinashDamera <avi.damera@gmail.com>
2023-02-10 18:47:46 +0530

    Responsive Site Changes
    Made the items responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 function onClickNavMenu() {
   document.getElementById("navicon").classList.toggle("nav-icon-open");
   document.getElementById("navmenu").classList.toggle("nav-menu-changed");
+
+  var pb = document.getElementById("cardContainer");
+
+  if (window.innerWidth < 1081) {
+    if (pb.style.marginTop === "20em") {
+      pb.style.marginTop = "0em";
+    } else {
+      pb.style.marginTop = "20em";
+    }
+  }
 }
